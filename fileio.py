bc596d554c3136dfbcb51573a8916399104f7224
Dean Haines <vbpupil@gmail.com>
2018-11-15 20:55:49 +0000

    Update fileio.py

--- fileio.py
+++ fileio.py
@@ -1,26 +1,33 @@
-import os
+from pathlib import Path
+import sys, os
+
+sys.tracebacklimit = 0
 
 
 class FileIO:
     def __init__(self, path):
         if isinstance(path, str):
-            self.path = path
+            tmp_file = Path(path)
+            if tmp_file.is_file():
+                self.path = path
+            else:
+                raise IOError('File does not exist')
         else:
-            raise ValueError('Incorrect TYPE, expecting str')
+            raise ValueError('Incorrect TYPE, string is required')
 
     def write(self, data, type='w'):
         try:
             file = open(self.path, type)
             file.write(str(data))
         except IOError:
-            raise IOError('Problem dealing with file: ' + self.path)
+            raise IOError('Problem writing to file: ' + self.path)
 
     def read(self):
         try:
             file = open(self.path, 'r')
             return file.read()
         except IOError:
-            raise IOError('Problem dealing with file: ' + self.path)
+            raise IOError('Problem reading from file: ' + self.path)
 
     def return_list(self):
         try:
@@ -28,3 +35,10 @@ class FileIO:
             return [line.rstrip() for line in src.readlines()]
         except IOError:
             raise IOError('Problem dealing with file: ' + self.path)
+
+    def create(self, path):
+        f = open(path, 'w+')
+        f.close()
+
+    def delete(self,path):
+        os.remove(path)
